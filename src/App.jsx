a8fdcee6a16e7d91b7971ffad1f39b3823d0ea64
Abubakar Shaikh (XERY) <64248752+abubakar-shaikh-dev@users.noreply.github.com>
2023-12-03 23:54:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Translate from "./Translate";
+
+export default function App() {
+  return (
+    <>
+      <Translate />
+      <h3>
+        Jumping foxes quickly vex the lazy dog with quirky whims, zapping joy
+        and excitement for quirky adventures.
+      </h3>
+    </>
+  );
+}
---
+++ src/Translate.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {useEffectOnce} from "./hooks/useEffectOnce"
+
+export default function Translate() {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false,
+      },
+      "google_translate_element"
+    );
+  };
+
+  useEffectOnce(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+
+  
+
+  return <div id="google_translate_element"></div>;
+}
